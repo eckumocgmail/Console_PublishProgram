7a47ebafe3b7ed47185200986d2e0a5f21542c11
Константин <eckumoc@gmail.com>
2022-09-04 06:17:23 +0300

    ╨б╨╛╨╖╨┤╨░╤О ╨╜╨╛╨▓╤Л╨╣ ╨┐╤А╨╛╨╡╨║╤В

---
+++ PublishProgramProgram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Console_PublishProgram
+{
+
+    /// <summary>
+    /// Программа публикует файлы 
+    /// </summary>
+    public class PublishProgramProgram
+    {
+        static void Main(string[] args) => Run(args);
+
+        public static void Run(params string[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
